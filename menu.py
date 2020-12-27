123503aec0d27bf4735b8ffe2a69ea9ce9da3393
Ефим <efim.smertin@yandex.ru>
2020-12-27 21:51:42 +0300

    sample menu 3

--- menu.py
+++ menu.py
@@ -4,8 +4,10 @@ import main
 pygame.init()
 pygame.display.set_caption("menu")
 SIZE = WIDTH, HEIGHT = 1200, 720
+
 screen = pygame.display.set_mode(SIZE)
 screen2 = pygame.Surface(screen.get_size())
+
 clock = pygame.time.Clock()
 
 text_coord = 0
@@ -16,7 +18,7 @@ circle_color = (255, 255, 255)
 circles = []
 directions = []
 
-radio = pygame.mixer.Sound('misc/music/music_in_menu.mp3')
+radio = pygame.mixer.Sound('misc/music.mp3')
 radio.play()
 turned_on = True
 
@@ -51,31 +53,31 @@ while running:
                         radio.stop()
                         turned_on = False
                     else:
-                        radio = pygame.mixer.Sound('misc/music/music_in_menu.mp3')
+                        radio = pygame.mixer.Sound('misc/music.mp3')
                         radio.play()
                         turned_on = True
 
-        screen2.fill((0, 0, 0))
+    screen2.fill((0, 0, 0))
 
-        for i in range(len(circles)):
-            for coord in (0, 1):
-                if circles[i][coord] >= SIZE[coord] - circle_radius or \
-                        circles[i][coord] <= circle_radius:
-                    directions[i][coord] *= -1
-                circles[i][coord] += directions[i][coord]
-            pygame.draw.circle(screen2, circle_color, circles[i], circle_radius, 0)
+    for i in range(len(circles)):
+        for coord in (0, 1):
+            if circles[i][coord] >= SIZE[coord] - circle_radius or \
+                    circles[i][coord] <= circle_radius:
+                directions[i][coord] *= -1
+            circles[i][coord] += directions[i][coord]
+        pygame.draw.circle(screen2, circle_color, circles[i], circle_radius, 0)
 
-        screen.blit(screen2, (0, 0))
+    screen.blit(screen2, (0, 0))
 
-        string_rendered = font.render('Murky gloom', 0, pygame.Color(255, 255, 240))
-        intro_rect = string_rendered.get_rect()
-        intro_rect.top = text_coord
-        text_coord = intro_rect.height
-        screen.blit(string_rendered, intro_rect)
+    string_rendered = font.render('Murky gloom', 0, pygame.Color(255, 255, 240))
+    intro_rect = string_rendered.get_rect()
+    intro_rect.top = text_coord
+    text_coord = intro_rect.height
+    screen.blit(string_rendered, intro_rect)
 
-        start_button = pygame.draw.rect(screen, (210, 200, 200), (350, 250, 530, 60))
-        quit_button = pygame.draw.rect(screen, (180, 150, 150), (350, 380, 530, 60))
-        off_button = pygame.draw.rect(screen, (150, 170, 170), (1110, 690, 75, 25))
+    start_button = pygame.draw.rect(screen, (210, 200, 200), (350, 250, 530, 60))
+    quit_button = pygame.draw.rect(screen, (180, 150, 150), (350, 380, 530, 60))
+    off_button = pygame.draw.rect(screen, (150, 170, 170), (1110, 690, 75, 25))
 
-        pygame.display.flip()
-        clock.tick(1000)
+    pygame.display.flip()
+    clock.tick(500)
